e16e492996e776eb9e34817fe36efb34f1134b41
elayneolima <elayne04lima@gmail.com>
2024-09-25 17:56:19 -0300

    adicionando interface a listagem de ctt

---
+++ MeuSiteMVC/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuSiteMVC.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
